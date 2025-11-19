f3e51925f16f7abc7010f940b299251568215752
Mads <madsleander@outlook.com>
2025-11-19 20:23:32 +0100

    Upload script

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-- FX Information
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+
+-- Resource Information
+name 'ox_inv_img_validator'
+author 'Mads'
+version '1.0.0'
+description 'A script for validating item images in ox_inventory'
+
+-- Manifest
+server_script 'server.lua'
+server_only 'yes'
